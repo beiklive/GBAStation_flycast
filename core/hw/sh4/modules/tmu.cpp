flyinghead/flycast

commit 05d51cc35c57b9762e7bf12cad532a6003a2f556

sh4: timer underflow lost if TCOR is 0 or very small
Fixes DreamShell boot

core/hw/sh4/modules/tmu.cpp
@@ -233,7 +233,7 @@ static int sched_tmu_cb(int ch, int sch_cycl, int jitter)
 
 			//schedule next trigger by writing the TCNT register
 			u32 tcor = TMU_TCOR(ch);
-			write_TMU_TCNTch(ch, tcor + tcnt);
+			write_TMU_TCNTch(ch, std::max((s32)(tcor + tcnt), 0));
 		}
 		else {
 			
@@ -307,7 +307,6 @@ void tmu_reset(bool hard)
 	}
 	TMU_TOCR=TMU_TSTR=0;
 	TMU_TCOR(0) = TMU_TCOR(1) = TMU_TCOR(2) = 0xffffffff;
-//	TMU_TCNT(0) = TMU_TCNT(1) = TMU_TCNT(2) = 0xffffffff;
 	TMU_TCR(0) = TMU_TCR(1) = TMU_TCR(2) = 0;
 
 	UpdateTMUCounts(0);

beiklive/GBAStation_flycast

commit 8b6d302ae6e295652334723b248abbc705c4f33d

sh4: fix TMU regression
introduced by 05d51cc35c57b9762e7bf12cad532a6003a2f556
fixes SA2 running too fast

core/hw/sh4/modules/tmu.cpp
@@ -233,7 +233,8 @@ static int sched_tmu_cb(int ch, int sch_cycl, int jitter)
 
 			//schedule next trigger by writing the TCNT register
 			u32 tcor = TMU_TCOR(ch);
-			write_TMU_TCNTch(ch, std::max((s32)(tcor + tcnt), 0));
+			// Don't miss an underflow if tcor is less than -tcnt
+			write_TMU_TCNTch(ch, (u32)std::max<s64>((u64)tcor + (s32)tcnt, 0));
 		}
 		else {
 			