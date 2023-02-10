flyinghead/flycast

commit e18a4afcbbc671fd31e661e181f4fa25fa6940a3

vk and pvr fixes. Renderer::Process returns void. N2 light 0 is no-light
pvr: Heap use after free due to PolyParam vector reallocation
vk: fix wrong buffer size calculation
Renderer::Process now returns void since it can't fail anymore
naomi2: light model at index 0 is "no light"
dx9, gles: throw if naomi 2 not supported

core/rend/gles/naomi2.h
@@ -127,62 +127,59 @@ void setN2Uniforms(const PolyParam *pp, ShaderType *shader, const rend_context& 
 	if (pp->lightModel != shader->lastLightModel)
 	{
 		shader->lastLightModel = pp->lightModel;
-		if (pp->lightModel != -1)
+		const N2LightModel *const lightModel = &ctx.lightModels[pp->lightModel];
+		for (int vol = 0; vol < 2; vol++)
 		{
-			const N2LightModel *const lightModel = &ctx.lightModels[pp->lightModel];
+			glUniform1i(shader->ambientMaterialBase[vol], lightModel->ambientMaterialBase[vol]);
+			glUniform1i(shader->ambientMaterialOffset[vol], lightModel->ambientMaterialOffset[vol]);
+			glUniform4fv(shader->ambientBase[vol], 1, lightModel->ambientBase[vol]);
+			glUniform4fv(shader->ambientOffset[vol], 1, lightModel->ambientOffset[vol]);
+		}
+		glUniform1i(shader->useBaseOver, lightModel->useBaseOver);
+		glUniform1i(shader->bumpId0, lightModel->bumpId1);
+		glUniform1i(shader->bumpId1, lightModel->bumpId2);
+
+		glUniform1i(shader->lightCount, lightModel->lightCount);
+		for (int i = 0; i < lightModel->lightCount; i++)
+		{
+			const N2Light& light = lightModel->lights[i];
+			glUniform1i(shader->lights[i].parallel, light.parallel);
+
+			glUniform4fv(shader->lights[i].color, 1, light.color);
+			glUniform4fv(shader->lights[i].direction, 1, light.direction);
+			glUniform4fv(shader->lights[i].position, 1, light.position);
+
 			for (int vol = 0; vol < 2; vol++)
 			{
-				glUniform1i(shader->ambientMaterialBase[vol], lightModel->ambientMaterialBase[vol]);
-				glUniform1i(shader->ambientMaterialOffset[vol], lightModel->ambientMaterialOffset[vol]);
-				glUniform4fv(shader->ambientBase[vol], 1, lightModel->ambientBase[vol]);
-				glUniform4fv(shader->ambientOffset[vol], 1, lightModel->ambientOffset[vol]);
+				glUniform1i(shader->lights[i].diffuse[vol], light.diffuse[vol]);
+				glUniform1i(shader->lights[i].specular[vol], light.specular[vol]);
 			}
-			glUniform1i(shader->useBaseOver, lightModel->useBaseOver);
-			glUniform1i(shader->bumpId0, lightModel->bumpId1);
-			glUniform1i(shader->bumpId1, lightModel->bumpId2);
+			glUniform1i(shader->lights[i].routing, light.routing);
+			glUniform1i(shader->lights[i].dmode, light.dmode);
+			glUniform1i(shader->lights[i].smode, light.smode);
+			glUniform1i(shader->lights[i].distAttnMode, light.distAttnMode);
 
-			glUniform1i(shader->lightCount, lightModel->lightCount);
-			for (int i = 0; i < lightModel->lightCount; i++)
-			{
-				const N2Light& light = lightModel->lights[i];
-				glUniform1i(shader->lights[i].parallel, light.parallel);
-
-				glUniform4fv(shader->lights[i].color, 1, light.color);
-				glUniform4fv(shader->lights[i].direction, 1, light.direction);
-				glUniform4fv(shader->lights[i].position, 1, light.position);
-
-				for (int vol = 0; vol < 2; vol++)
-				{
-					glUniform1i(shader->lights[i].diffuse[vol], light.diffuse[vol]);
-					glUniform1i(shader->lights[i].specular[vol], light.specular[vol]);
-				}
-				glUniform1i(shader->lights[i].routing, light.routing);
-				glUniform1i(shader->lights[i].dmode, light.dmode);
-				glUniform1i(shader->lights[i].smode, light.smode);
-				glUniform1i(shader->lights[i].distAttnMode, light.distAttnMode);
-
-				glUniform1f(shader->lights[i].attnDistA, light.attnDistA);
-				glUniform1f(shader->lights[i].attnDistB, light.attnDistB);
-				glUniform1f(shader->lights[i].attnAngleA, light.attnAngleA);
-				glUniform1f(shader->lights[i].attnAngleB, light.attnAngleB);
-			}
+			glUniform1f(shader->lights[i].attnDistA, light.attnDistA);
+			glUniform1f(shader->lights[i].attnDistB, light.attnDistB);
+			glUniform1f(shader->lights[i].attnAngleA, light.attnAngleA);
+			glUniform1f(shader->lights[i].attnAngleB, light.attnAngleB);
 		}
-		else
+	}
+	else
+	{
+		float white[] { 1.f, 1.f, 1.f, 1.f };
+		float black[4]{};
+		for (int vol = 0; vol < 2; vol++)
 		{
-			float white[] { 1.f, 1.f, 1.f, 1.f };
-			float black[4]{};
-			for (int vol = 0; vol < 2; vol++)
-			{
-				glUniform1i(shader->ambientMaterialBase[vol], 0);
-				glUniform1i(shader->ambientMaterialOffset[vol], 0);
-				glUniform4fv(shader->ambientBase[vol], 1, white);
-				glUniform4fv(shader->ambientOffset[vol], 1, black);
-			}
-			glUniform1i(shader->useBaseOver, 0);
-			glUniform1i(shader->lightCount, 0);
-			glUniform1i(shader->bumpId0, -1);
-			glUniform1i(shader->bumpId1, -1);
+			glUniform1i(shader->ambientMaterialBase[vol], 0);
+			glUniform1i(shader->ambientMaterialOffset[vol], 0);
+			glUniform4fv(shader->ambientBase[vol], 1, white);
+			glUniform4fv(shader->ambientOffset[vol], 1, black);
 		}
+		glUniform1i(shader->useBaseOver, 0);
+		glUniform1i(shader->lightCount, 0);
+		glUniform1i(shader->bumpId0, -1);
+		glUniform1i(shader->bumpId1, -1);
 	}
 	glUniform1i(shader->bumpMapping, pp->pcw.Texture == 1 && pp->tcw.PixelFmt == PixelBumpMap);
 }

core/emulator.cpp
@@ -502,9 +502,6 @@ void Emulator::loadGame(const char *path, LoadProgress *progress)
 		dc_reset(true);
 		memset(&settings.network.md5, 0, sizeof(settings.network.md5));
 
-		if (settings.platform.isNaomi2() && config::RendererType == RenderType::DirectX9)
-			throw FlycastException("DirectX 9 doesn't support Naomi 2 games. Select a different graphics API");
-
 		if (settings.platform.isConsole())
 		{
 			if (settings.content.path.empty())

core/hw/pvr/Renderer_if.cpp
@@ -183,28 +183,24 @@ private:
 			retro_resize_renderer(_pvrrc->rend.framebufferWidth, _pvrrc->rend.framebufferHeight,
 					getOutputFramebufferAspectRatio());
 #endif
-		bool proc;
 		{
 			FC_PROFILE_SCOPE_NAMED("Renderer::Process");
-			proc = renderer->Process(_pvrrc);
+			renderer->Process(_pvrrc);
 		}
 
-		if (!proc || renderToScreen)
+		if (renderToScreen)
 			// If rendering to texture or in full framebuffer emulation, continue locking until the frame is rendered
 			renderEnd.Set();
 		rend_allow_rollback();
-		if (proc)
 		{
-			{
-				FC_PROFILE_SCOPE_NAMED("Renderer::Render");
-				renderer->Render();
-			}
-
-			if (!renderToScreen)
-				renderEnd.Set();
-			else if (config::DelayFrameSwapping && fb_w_cur == FB_R_SOF1)
-				present();
+			FC_PROFILE_SCOPE_NAMED("Renderer::Render");
+			renderer->Render();
 		}
+
+		if (!renderToScreen)
+			renderEnd.Set();
+		else if (config::DelayFrameSwapping && fb_w_cur == FB_R_SOF1)
+			present();
 
 		//clear up & free data ..
 		FinishRender(_pvrrc);

core/hw/pvr/Renderer_if.h
@@ -56,7 +56,7 @@ struct Renderer
 	virtual bool Init() = 0;
 	virtual void Term() = 0;
 
-	virtual bool Process(TA_context *ctx) = 0;
+	virtual void Process(TA_context *ctx) = 0;
 	virtual bool Render() = 0;
 	virtual void RenderFramebuffer(const FramebufferInfo& info) = 0;
 	virtual bool RenderLastFrame() { return false; }

core/hw/pvr/ta.h
@@ -14,7 +14,7 @@ void ta_vtx_SoftReset();
 void DYNACALL ta_vtx_data32(const SQBuffer *data);
 void ta_vtx_data(const SQBuffer *data, u32 size);
 
-bool ta_parse(TA_context *ctx, bool primRestart);
+void ta_parse(TA_context *ctx, bool primRestart);
 
 class TaTypeLut
 {

core/hw/pvr/ta_vtx.cpp
@@ -154,10 +154,10 @@ protected:
 	static ModTriangle* lmr;
 
 	static u32 CurrentList;
-	static PolyParam* CurrentPP;
 	static TaListFP *VertexDataFP;
 public:
 	static std::vector<PolyParam> *CurrentPPlist;
+	static PolyParam* CurrentPP;
 	static TaListFP* TaCmd;
 	static bool fetchTextures;
 };
@@ -633,12 +633,10 @@ private:
 
 		if (CurrentPP->count > 0)
 		{
-			CurrentPPlist->emplace_back();
-			PolyParam* d_pp = &CurrentPPlist->back();
-			*d_pp = *CurrentPP;
-			CurrentPP = d_pp;
-			d_pp->first = vd_rc.verts.size();
-			d_pp->count = 0;
+			CurrentPPlist->push_back(*CurrentPP);
+			CurrentPP = &CurrentPPlist->back();
+			CurrentPP->first = vd_rc.verts.size();
+			CurrentPP->count = 0;
 		}
 	}
 	
@@ -954,7 +952,6 @@ private:
 	//Sprites
 	static void AppendSpriteParam(TA_SpriteParam* spr)
 	{
-		//printf("Sprite\n");
 		PolyParam* d_pp = CurrentPP;
 		if (CurrentPP == NULL || CurrentPP->count != 0)
 		{
@@ -1087,9 +1084,8 @@ private:
 
 		update_fz(cv[0].z);
 
-		CurrentPPlist->emplace_back();
+		CurrentPPlist->push_back(*CurrentPP);
 		PolyParam *d_pp = &CurrentPPlist->back();
-		*d_pp = *CurrentPP;
 		CurrentPP = d_pp;
 		d_pp->first = vd_rc.verts.size();
 		d_pp->count = 0;
@@ -1298,13 +1294,12 @@ static void ta_parse_naomi2(TA_context* ctx, bool primRestart)
 	ctx->rend.fb_Y_CLIP.max = std::min(ctx->rend.fb_Y_CLIP.max, ymax + 31);
 }
 
-bool ta_parse(TA_context *ctx, bool primRestart)
+void ta_parse(TA_context *ctx, bool primRestart)
 {
 	if (settings.platform.isNaomi2())
 		ta_parse_naomi2(ctx, primRestart);
 	else
 		ta_parse_vdrc(ctx, primRestart);
-	return true;
 }
 
 //
@@ -1329,6 +1324,7 @@ const float defaultProjMat[] {
 
 constexpr int IdentityMatIndex = 0;
 constexpr int DefaultProjMatIndex = 1;
+constexpr int NoLightIndex = 0;
 
 static void setDefaultMatrices()
 {
@@ -1339,6 +1335,12 @@ static void setDefaultMatrices()
 	}
 }
 
+static void setDefaultLight()
+{
+	if (ta_ctx->rend.lightModels.empty())
+		ta_ctx->rend.lightModels.emplace_back();
+}
+
 void ta_add_poly(const PolyParam& pp)
 {
 	verify(ta_ctx != nullptr);
@@ -1347,6 +1349,7 @@ void ta_add_poly(const PolyParam& pp)
 	BaseTAParser::startList(pp.pcw.ListType);
 
 	BaseTAParser::CurrentPPlist->push_back(pp);
+	BaseTAParser::CurrentPP = nullptr; // might be invalidated
 	n2CurrentPP = &BaseTAParser::CurrentPPlist->back();
 	n2CurrentPP->first = ta_ctx->rend.verts.size();
 	n2CurrentPP->count = 0;
@@ -1358,6 +1361,9 @@ void ta_add_poly(const PolyParam& pp)
 		n2CurrentPP->normalMatrix = IdentityMatIndex;
 	if (n2CurrentPP->projMatrix == -1)
 		n2CurrentPP->projMatrix = DefaultProjMatIndex;
+	setDefaultLight();
+	if (n2CurrentPP->lightModel == -1)
+		n2CurrentPP->lightModel = NoLightIndex;
 	vd_ctx = nullptr;
 }
 
@@ -1413,6 +1419,7 @@ int ta_add_matrix(const float *matrix)
 
 int ta_add_light(const N2LightModel& light)
 {
+	setDefaultLight();
 	ta_ctx->rend.lightModels.push_back(light);
 	return ta_ctx->rend.lightModels.size() - 1;
 }

core/rend/dx11/dx11_naomi2.cpp
@@ -434,15 +434,7 @@ void Naomi2Helper::setConstants(const PolyParam& pp, u32 polyNumber, const rend_
 	if (pp.lightModel != lastModel)
 	{
 		lastModel = pp.lightModel;
-		if (pp.lightModel != -1)
-		{
-			setConstBuffer(lightConstantsBuffer, ctx.lightModels[pp.lightModel]);
-		}
-		else
-		{
-			N2LightModel lightModel{};
-			setConstBuffer(lightConstantsBuffer, lightModel);
-		}
+		setConstBuffer(lightConstantsBuffer, ctx.lightModels[pp.lightModel]);
 		deviceContext->VSSetConstantBuffers(2, 1, &lightConstantsBuffer.get());
 	}
 }

core/rend/dx11/dx11_renderer.cpp
@@ -301,13 +301,13 @@ BaseTextureCacheData *DX11Renderer::GetTexture(TSP tsp, TCW tcw)
 	return tf;
 }
 
-bool DX11Renderer::Process(TA_context* ctx)
+void DX11Renderer::Process(TA_context* ctx)
 {
 	if (KillTex)
 		texCache.Clear();
 	texCache.Cleanup();
 
-	return ta_parse(ctx, true);
+	ta_parse(ctx, true);
 }
 
 void DX11Renderer::configVertexShader()

beiklive/GBAStation_flycast

commit 1ecc23bee3d414a7d54e3fbf9e63431c17abc88a

gl: fix naomi2 lights regression
regression introduced by e18a4afcbbc671fd31e661e181f4fa25fa6940a3

core/rend/gles/naomi2.h
@@ -31,6 +31,7 @@ void resetN2UniformCache(ShaderType *shader)
 	shader->lastMvMat = -1;
 	shader->lastProjMat = -1;
 	shader->lastLightModel = -1;
+	shader->lastNormalMat = -1;
 }
 
 template<typename ShaderType>
@@ -164,22 +165,6 @@ void setN2Uniforms(const PolyParam *pp, ShaderType *shader, const rend_context& 
 			glUniform1f(shader->lights[i].attnAngleA, light.attnAngleA);
 			glUniform1f(shader->lights[i].attnAngleB, light.attnAngleB);
 		}
-	}
-	else
-	{
-		float white[] { 1.f, 1.f, 1.f, 1.f };
-		float black[4]{};
-		for (int vol = 0; vol < 2; vol++)
-		{
-			glUniform1i(shader->ambientMaterialBase[vol], 0);
-			glUniform1i(shader->ambientMaterialOffset[vol], 0);
-			glUniform4fv(shader->ambientBase[vol], 1, white);
-			glUniform4fv(shader->ambientOffset[vol], 1, black);
-		}
-		glUniform1i(shader->useBaseOver, 0);
-		glUniform1i(shader->lightCount, 0);
-		glUniform1i(shader->bumpId0, -1);
-		glUniform1i(shader->bumpId1, -1);
 	}
 	glUniform1i(shader->bumpMapping, pp->pcw.Texture == 1 && pp->tcw.PixelFmt == PixelBumpMap);
 }