flyinghead/flycast

commit 942224c68533b801b9987101853400d02a830d1c

in single threaded, only defer emu stop on android
on other platforms, render() won't be called => audio not terminated,
rom files not saved, pause event missed

unprotect memory and reset memwatch before loading a state

core/emulator.cpp
@@ -680,9 +680,15 @@ void Emulator::stop()
 	}
 	else
 	{
+#ifdef __ANDROID__
 		// defer stopping audio until after the current frame is finished
 		// normally only useful on android due to multithreading
 		stopRequested = true;
+#else
+		TermAudio();
+		SaveRomFiles();
+		EventManager::event(Event::Pause);
+#endif
 	}
 }
 
@@ -751,6 +757,8 @@ void dc_loadstate(Deserializer& deser)
 #if FEAT_SHREC != DYNAREC_NONE
 	bm_Reset();
 #endif
+	memwatch::unprotect();
+	memwatch::reset();
 
 	dc_deserialize(deser);
 

beiklive/GBAStation_flycast

commit 19f9d4eca6f68188e0a16261ad32853b7f17c6d1

texture corruption after load state regression. wince depth scale change
Texture weren't updated after loading a state. Regression introduced by
942224c68533b801b9987101853400d02a830d1c.
Change Windows CE depth scale from 0.1 to 0.01 to fix black screen FMV
on AMD and Intel GPUs. Issue #973

core/emulator.cpp
@@ -45,6 +45,7 @@
 #include <chrono>
 
 settings_t settings;
+constexpr float WINCE_DEPTH_SCALE = 0.01f;
 
 static void loadSpecialSettings()
 {
@@ -57,7 +58,7 @@ static void loadSpecialSettings()
 				|| prod_id == "T26702N") // PBA Tour Bowling 2001
 		{
 			INFO_LOG(BOOT, "Enabling Full MMU and Extra depth scaling for Windows CE game");
-			config::ExtraDepthScale.override(0.1f); // taxi 2 needs 0.01 for FMV (amd, per-tri)
+			config::ExtraDepthScale.override(WINCE_DEPTH_SCALE);
 			config::FullMMU.override(true);
 			if (!config::ForceWindowsCE)
 				config::ForceWindowsCE.override(true);
@@ -732,7 +733,7 @@ void loadGameSpecificSettings()
 
 	if (config::ForceWindowsCE)
 	{
-		config::ExtraDepthScale.override(0.1f);
+		config::ExtraDepthScale.override(WINCE_DEPTH_SCALE);
 		config::FullMMU.override(true);
 	}
 }
@@ -770,6 +771,7 @@ void dc_loadstate(Deserializer& deser)
 
 	mmu_set_state();
 	sh4_cpu.ResetCache();
+	KillTex = true;
 }
 
 void Emulator::setNetworkState(bool online)