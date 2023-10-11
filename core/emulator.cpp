flyinghead/flycast

commit cc1b45b7cc4c3e0aa6438979efcd7972016e2146

texture corruption after load state regression. wince depth scale change
Texture weren't updated after loading a state. Regression introduced by
942224c68533b801b9987101853400d02a830d1c.
Change Windows CE depth scale from 0.1 to 0.01 to fix black screen FMV
on AMD and Intel GPUs. Issue #973

core/emulator.cpp
@@ -45,6 +45,7 @@
 #include <chrono>
 
 settings_t settings;
+constexpr float WINCE_DEPTH_SCALE = 0.01f;
 
 static void loadSpecialSettings()
 {
@@ -57,7 +58,7 @@ static void loadSpecialSettings()
 				|| prod_id == "T26702N") // PBA Tour Bowling 2001
 		{
 			INFO_LOG(BOOT, "Enabling Full MMU and Extra depth scaling for Windows CE game");
-			config::ExtraDepthScale.override(0.1f); // taxi 2 needs 0.01 for FMV (amd, per-tri)
+			config::ExtraDepthScale.override(WINCE_DEPTH_SCALE);
 			config::FullMMU.override(true);
 			if (!config::ForceWindowsCE)
 				config::ForceWindowsCE.override(true);
@@ -732,7 +733,7 @@ void loadGameSpecificSettings()
 
 	if (config::ForceWindowsCE)
 	{
-		config::ExtraDepthScale.override(0.1f);
+		config::ExtraDepthScale.override(WINCE_DEPTH_SCALE);
 		config::FullMMU.override(true);
 	}
 }
@@ -770,6 +771,7 @@ void dc_loadstate(Deserializer& deser)
 
 	mmu_set_state();
 	sh4_cpu.ResetCache();
+	KillTex = true;
 }
 
 void Emulator::setNetworkState(bool online)

beiklive/GBAStation_flycast

commit 18dbe514defa7e518fbfe101a0371c80590cfce5

set extra depth scale back to 0.1 for Caesars Palace 2000
Fixes pause menu background transparency in video keno game.
Regression introduced by cc1b45b7cc4c3e0aa6438979efcd7972016e2146
Issue #1236

core/emulator.cpp
@@ -153,7 +153,9 @@ static void loadSpecialSettings()
 			config::ExtraDepthScale.override(1e26f);
 		}
 		// Test Drive V-Rally
-		else if (prod_id == "T15110N" || prod_id == "T15105D 50")
+		else if (prod_id == "T15110N" || prod_id == "T15105D 50"
+				// Caesars Palace 2000
+				|| prod_id == "T-12504N" || prod_id == "12502D-50")
 		{
 			INFO_LOG(BOOT, "Enabling Extra depth scaling for game %s", prod_id.c_str());
 			config::ExtraDepthScale.override(0.1f);
@@ -807,7 +809,7 @@ void loadGameSpecificSettings()
 	// Reload per-game settings
 	config::Settings::instance().load(true);
 
-	if (config::ForceWindowsCE)
+	if (config::ForceWindowsCE && !config::ExtraDepthScale.isReadOnly())
 		config::ExtraDepthScale.override(WINCE_DEPTH_SCALE);
 }
 